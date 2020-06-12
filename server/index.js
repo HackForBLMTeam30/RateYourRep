10944dd897f5f6d9bce1b97e7fc34ff28439ac4f
Ann-MarieKemp <aedalenb@gmail.com>
2020-06-12 18:29:05 -0400

    added react router

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '..//build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('Rating Reps on port: ' + port);
