3fb6583447480dab1edf86ee54b1ac293f1aa906
Kathleen McDaniel <kathleen_mc.daniel@yahoo.com>
2020-06-13 12:24:39 -0400

    fix: removed serving static files since create-react-app performs this action for us; added paths for routes to use

--- server/index.js
+++ server/index.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const path = require('path');
 const db = require('./db');
 
 const app = express();
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../build')));
-
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../build/index.html'));
-});
+app.use('/auth', require('./auth'))
+app.use('/api', require('./api'))
 
 const port = process.env.PORT || 5000;
 
