f90ff09e4e60f204fbe65e6c7c0715418d4827b9
electrone901 <carbajalluis@rocketmail.com>
2020-06-13 16:31:01 -0400

    rating: add post router

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 module.exports = router;
 
+
 // Put your route paths here
 router.use('/users', require('./users'));
 router.use('/legislators', require('./legislators'));
+router.use('/rating', require('./rating'));
 
 router.use((req, res, next) => {
     const error = new Error('Not Found');
