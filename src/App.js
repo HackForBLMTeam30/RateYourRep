440e3d18ce3f6f27cfe658eef5b10c15fabe6d8d
eliskim27 <eliskim27@gmail.com>
2020-06-13 09:57:35 -0400

    initial commit + material ui core

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          test test test
         </a>
       </header>
     </div>
