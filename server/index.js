e15f6d5164918f8c9e8cd984442befabbba35320
Ann-MarieKemp <aedalenb@gmail.com>
2020-06-12 23:56:04 -0400

    fixed typo in server/index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const db = require('./db');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '..//build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
