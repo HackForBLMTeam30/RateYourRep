1f6125ce462ff1593592876e25939db5287dbcf3
Ann-MarieKemp <aedalenb@gmail.com>
2020-06-13 21:49:00 -0400

    feat: added get single legislator endpoint

--- server/api/legislators.js
+++ server/api/legislators.js
@@ -1,50 +1,73 @@
 const router = require('express').Router();
 const axios = require('axios');
 const apiKey = require('../../config/key').key;
-const {Legislator} = require('../db/models')
+const { Legislator, Rating } = require('../db/models');
 
-router.get('/', async(req, res, next) => {
+router.get('/', async (req, res, next) => {
+  const inputAddress = req.query.address;
+  try {
+    const { data } = await axios.get(
+      `https://www.googleapis.com/civicinfo/v2/representatives?key=${apiKey}&address=${inputAddress}`
+    );
+    //parses address into one string
+    data.officials.map((legislator) => {
+      if (legislator.address) {
+        let address = legislator.address[0];
+        const { line1, city, state, zip } = address;
+        legislator.address = `${line1} ${city} ${state} ${zip}`;
+      }
+    });
+    let officialsArray = [];
+    data.offices.map((office) => {
+      const { name, officialIndices } = office;
+      officialIndices.forEach((office) => {
+        let official = data.officials[office];
+        if (official.name !== null) {
+          let officialObject = { ...official, role: name };
+          officialsArray.push(officialObject);
+        }
+      });
+    });
+    officialsArray.forEach(async (official) => {
+      console.log(official.address);
+      const person = await Legislator.findOne(
+        { where: { name: official.name, role: official.role } },
+        {
+          include: [
+            {
+              model: Rating,
+            },
+          ],
+        }
+      );
+      if (!person) {
+        Legislator.create(official);
+      }
+    });
 
-    const inputAddress = req.query.address;
-    try {
-        const { data } = await axios.get(
-            `https://www.googleapis.com/civicinfo/v2/representatives?key=${apiKey}&address=${inputAddress}`
-        );
-        //parses address into one string
-        data.officials.map((legislator) => {
-            if(legislator.address){
-                let address = legislator.address[0]
-                const {line1, city, state, zip} = address
-                legislator.address = `${line1} ${city} ${state} ${zip}`
-            }
-        })
-        let officialsArray =  []
-        data.offices.map((office) => {
-            const {name, officialIndices} = office
-            officialIndices.forEach((office) =>{
-                let official = data.officials[office]
-                if(official.name !== null){
-                let officialObject = {...official, role: name}
-               officialsArray.push(officialObject)
-                }
-            })
-        })
-        officialsArray.forEach(async(official) => {
-            const person = await Legislator.upsert(official)
-        })
-
-        res.json(officialsArray);
-
-    } catch (error) {
-        next(error);
-    }
+    res.json(officialsArray);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id', async(req, res, next) => {
-    try{
-    let id = req.params.id,
-    console.log(id)
-    }
-})
+router.get('/:id', async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    const legislator = await Legislator.findOne(
+      { where: { id } },
+      {
+        include: [
+          {
+            model: Rating,
+          },
+        ],
+      }
+    );
+    res.send(legislator);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- server/api/rating.js
+++ server/api/rating.js
@@ -3,84 +3,81 @@ const Rating = require('../db/models/rating');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('postgres://localhost:5432/rateyourrep');
 
-class AvgRating {
-    constructor(total, count) {
+// class AvgRating {
+//     constructor(total, count) {
 
-        this.total = total;
-        this.count = count;
-    }
-    addRating(value) {
-        this.count++;
-        this.total += value
-    }
-    updateRating(prevRating, newRating) {
-        this.total -= prevRating
-        this.total += newR
-    }
-    getRating() {
-        return this.total / this.count
-    }
+//         this.total = total;
+//         this.count = count;
+//     }
+//     addRating(value) {
+//         this.count++;
+//         this.total += value
+//     }
+//     updateRating(prevRating, newRating) {
+//         this.total -= prevRating
+//         this.total += newR
+//     }
+//     getRating() {
+//         return this.total / this.count
+//     }
 
-}
+// }
 
+// class LegislatorRating {
+//     constructor() {
+//         this.totalRatings = new AvgRating();
+//         this.blackRatings = new AvgRating();
+//     }
 
-class LegislatorRating {
-    constructor() {
-        this.totalRatings = new AvgRating();
-        this.blackRatings = new AvgRating();
-    }
+// }
 
-}
+// const legislators = {}
 
-const legislators = {}
+// router.post('/', async(req, res, next) => {
+//     try {
+//         const newRating = await Rating.create(req.body);
+//         res.json(newRating);
 
-router.post('/', async(req, res, next) => {
-    try {
-        const newRating = await Rating.create(req.body);
-        res.json(newRating);
+//         if (user.isBlack) {
+//             legislators[legislators.name].blackRatings.addRating()
+//         }
+//         legislators[legislators.name].totalRatings.addRating()
 
+//         return []
 
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
-        if (user.isBlack) {
-            legislators[legislators.name].blackRatings.addRating()
-        }
-        legislators[legislators.name].totalRatings.addRating()
-
-        return []
-
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get('/', async(req, res, next) => {
-    try {
-        const id = req.body.id;
-        const [ratingAvg, metadata] = await Rating.findAll({
-            where: { legislatorId: id },
-            attributes: [
-                'legislatorId', [
-                    sequelize.fn('AVG', sequelize.col('transparency')),
-                    'transparencyCount',
-                ],
-                [
-                    sequelize.fn('AVG', sequelize.col('publicEngagement')),
-                    'publicEngagementCount',
-                ],
-                [
-                    sequelize.fn('AVG', sequelize.col('alignWithValues')),
-                    'alignWithValuesCount',
-                ],
-            ],
-            group: 'legislatorId',
-            order: [
-                [sequelize.fn('AVG', sequelize.col('legislatorId')), 'DESC']
-            ],
-        });
-        res.json(ratingAvg);
-    } catch (error) {
-        next(error);
-    }
-});
+// router.get('/', async(req, res, next) => {
+//     try {
+//         const id = req.body.id;
+//         const [ratingAvg, metadata] = await Rating.findAll({
+//             where: { legislatorId: id },
+//             attributes: [
+//                 'legislatorId', [
+//                     sequelize.fn('AVG', sequelize.col('transparency')),
+//                     'transparencyCount',
+//                 ],
+//                 [
+//                     sequelize.fn('AVG', sequelize.col('publicEngagement')),
+//                     'publicEngagementCount',
+//                 ],
+//                 [
+//                     sequelize.fn('AVG', sequelize.col('alignWithValues')),
+//                     'alignWithValuesCount',
+//                 ],
+//             ],
+//             group: 'legislatorId',
+//             order: [
+//                 [sequelize.fn('AVG', sequelize.col('legislatorId')), 'DESC']
+//             ],
+//         });
+//         res.json(ratingAvg);
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
 module.exports = router;
