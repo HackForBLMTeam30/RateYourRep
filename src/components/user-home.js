6673199ee7365a15740e57ee5444b2118d31ff56
Ann-MarieKemp <aedalenb@gmail.com>
2020-06-14 15:32:58 -0400

    styling: added styling to home page

--- src/components/user-home.js
+++ src/components/user-home.js
@@ -1,37 +1,54 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {Link, connect} from 'react-redux'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email} = props
+export const UserHome = (props) => {
+  const { email } = props;
 
   return (
     <div>
       <h3>Welcome, {email}</h3>
-    </div>
+      <div className="links-div">
+        <div className="nav-box">
+          <h4>Your Ratings</h4>
+          <button>
+            <Link className="link-fix" to="/ratings">
+              Get All Legislators
+            </Link>
+          </button>
+        </div>
 
-  
-   <Link to="/legislators">Get All Legislators</Link>
-  )
-}
+        <div className="nav-box">
+          <h4>Legislators</h4>
+          <button>
+            <Link className="link-fix" to="/legislators">
+              Get All Legislators
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    email: state.user.email
-  }
-}
+    email: state.user.email,
+  };
+};
 
-export default connect(mapState)(UserHome)
+export default connect(mapState)(UserHome);
 
 /**
  * PROP TYPES
  */
 UserHome.propTypes = {
-  email: PropTypes.string
-}
+  email: PropTypes.string,
+};
